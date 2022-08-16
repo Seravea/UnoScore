f96d897f660eef8b87de78edb28eff21a380de15
Seravea <romainpoyard@gmail.com>
2022-08-16 09:10:06 +0200

    Initial Commit

---
+++ UnoScore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnoScore
+//
+//  Created by Romain Poyard on 16/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worldHello test Commit!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UnoScore/UnoScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnoScoreApp.swift
+//  UnoScore
+//
+//  Created by Romain Poyard on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnoScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
